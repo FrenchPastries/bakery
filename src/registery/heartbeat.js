69083e39b61c8d89a3461c4711f7d58ed4bdec44
Guillaume Hivert <guillaume.hivert@outlook.com>
2019-02-08 16:29:27 +0100

    :art: Improve code structure

--- src/registery/heartbeat.js
+++ src/registery/heartbeat.js
@@ -2,7 +2,7 @@ const http = require('http')
 const registery = require('./registery')
 const TIMEOUT = parseInt(process.env.TIMEOUT_HEARTBEAT.trim(), 10)
 
-const ping = service => {
+const ping = (service) => {
   const address = service.address.split(':')
   const hostname = address[0]
   const port = parseInt(address[1])
@@ -11,37 +11,36 @@ const ping = service => {
     port,
     path: '/heartbeat',
     agent: false
-  }, (res) => {
-    console.log('Success ping at ' + service.address + ' (' + service.uuid + ')')
-    let data = ''
-    res.on('data', (d) => {
-      data += d
-    })
-    res.on('end', () => {
-      const dataRes = JSON.parse(data)
-      if (dataRes.uuid != service.uuid) {
-        deadService(service)
-      }
-    })
-  });
-  request.setTimeout(TIMEOUT, () => {
-    deadService(service)
-  });
-  request.on("error", () => {
-    deadService(service)
+  }, getPingResponse(service))
+  request.setTimeout(TIMEOUT, () => deadService(service))
+  request.on('error', () => deadService(service))
+}
+
+const getPingResponse = (service) => (res) => {
+  console.log(`Success ping at ${service.address} (${service.uuid})`)
+  let data = ''
+  res.on('data', (d) => data += d)
+  res.on('end', () => {
+    const dataRes = JSON.parse(data)
+    if (dataRes.uuid != service.uuid) {
+      deadService(service)
+    }
   })
 }
 
-const pingEveryServices = (registery) => {
-  const services = Object.values(registery.services)
-  // console.log(services)
-  services.reduce((acc, val) => acc.concat(Object.values(val)), []).forEach(ping)
+const getAllServices = ({ services }) => {
+  return Object
+    .values(services)
+    .reduce((acc, val) => acc.concat(Object.values(val)), [])
 }
 
-const deadService = (service) => {
-  console.log(service.name + ' at ' + service.address + ' iz ded!')
-  registery.deleteDeadService(service.uuid)
+const pingEveryServices = (registery) => {
+  getAllServices(registery).forEach(ping)
+}
 
+const deadService = ({ name, address, uuid }) => {
+  console.log(`${name} at ${address} iz ded!`)
+  registery.deleteDeadService(uuid)
 }
 
 module.exports = {
