64207c25d2ce6d7e682de4acf82f415bdafbe0cd
Guillaume Hivert <guillaume.hivert@outlook.com>
2020-02-19 10:55:19 +0100

    :sparkles: Properly integrate backoffice

--- src/Bakery.js
+++ src/Bakery.js
@@ -2,6 +2,8 @@ const MilleFeuille = require('@frenchpastries/millefeuille')
 const { response } = require('@frenchpastries/millefeuille/response')
 const { post, notFound, routes } = require('@frenchpastries/assemble')
 const { jsonResponse, parseJSONBody } = require('@frenchpastries/arrange')
+const fs = require('fs').promises
+const path = require('path')
 
 const Registry = require('./registry/registry')
 const heartbeat = require('./registry/heartbeat')
@@ -31,14 +33,43 @@ const pingServices = (heartbeatInterval, heartbeatTimeout, registry) => {
   )
 }
 
-const interceptGet = handler => request => {
+const getStaticFiles = async request => {
+  if (request.url.pathname === '/') {
+    const htmlFilePath = path.resolve(__dirname, '../backoffice/build/index.html')
+    const htmlFile = await fs.readFile(htmlFilePath, 'utf8')
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: htmlFile,
+    }
+  } else {
+    const relativePath = '../backoffice/build' + request.url.pathname
+    const filePath = path.resolve(__dirname, relativePath)
+    const file = await fs.readFile(filePath, 'utf8')
+    return {
+      statusCode: 200,
+      body: file,
+    }
+  }
+}
+
+const interceptGet = handler => async request => {
   if (request.method === 'GET') {
-    if (process.env.NODE_ENV !== 'production') {
+    if (process.env.BAKERY_DEVELOPMENT) {
       return {
         statusCode: 302,
         headers: {
           Location: 'http://localhost:3006',
-        }
+        },
+      }
+    } else if (process.env.NODE_ENV === 'development') {
+      try {
+        return await getStaticFiles(request)
+      } catch (error) {
+        console.error(error)
+        return handleNotFound()
       }
     }
   } else {
